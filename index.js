0916a2e749d55ece6ef952f331c871fdc7ac2f22
chara <maximiliano.charamello@gmail.com>
2023-07-12 11:17:41 -0300

    1era preEntrega - Charamello

--- index.js
+++ index.js
@@ -1,59 +1,61 @@
-// Declaro un array con los datos de las pizzas y sus precios
-const pizzas = [
-    { nombre: 'Margarita', precio: 300 },
-    { nombre: 'Pepperoni', precio: 200 },
-    { nombre: 'Hawaiana', precio: 500 },
-    { nombre: 'Vegetariana', precio: 400 }
-  ];
-  
-  let carrito = [];
-  
-  let seleccion = prompt("¡Bienvenido a Mistica Pizza! ¿Deseas ordenar una pizza? (SI/NO)");
-  
-  while (seleccion.toLowerCase() !== "si" && seleccion.toLowerCase() !== "no") {
-    console.log("Por favor, ingresa solo SI o NO.");
-    seleccion = prompt("¿Deseas ordenar una pizza? (SI/NO)");
-  }
-  
-  if (seleccion.toLowerCase() === "si") {
-    console.log("A continuación te mostramos nuestro menú de pizzas:");
-    pizzas.forEach((pizza, index) => {
-      console.log(`${index + 1}. ${pizza.nombre} - $${pizza.precio}`);
-    });
-  } else if (seleccion.toLowerCase() === "no") {
-    console.log("¡Esperamos verte pronto! ¡Buen día!");
-  }
-  
-  while (seleccion.toLowerCase() === "si") {
-    let opcionPizza = parseInt(prompt("Ingresa el número de la pizza que deseas ordenar:"));
-  
-    if (opcionPizza >= 1 && opcionPizza <= pizzas.length) {
-      let pizzaElegida = pizzas[opcionPizza - 1];
-      let cantidad = parseInt(prompt(`¿Cuántas ${pizzaElegida.nombre} deseas ordenar?`));
-  
-      if (cantidad > 0) {
-        let subtotal = pizzaElegida.precio * cantidad;
-        carrito.push({ pizza: pizzaElegida.nombre, cantidad, subtotal });
-        console.log(`Has agregado ${cantidad} ${pizzaElegida.nombre} al carrito.`);
-      } else {
-        console.log("La cantidad ingresada no es válida. Inténtalo nuevamente.");
+let total = 0;
+
+let seleccion = prompt("¡Bienvenido a Mistica Pizza! ¿Deseas ordenar una pizza? (SI/NO)");
+
+while (seleccion.toLowerCase() !== "si" && seleccion.toLowerCase() !== "no") {
+  console.log("Por favor, ingresa solo SI o NO.");
+  seleccion = prompt("¿Deseas ordenar una pizza? (SI/NO)");
+}
+
+if (seleccion.toLowerCase() === "si") {
+  console.log("A continuación te mostramos nuestro menú de pizzas:");
+  console.log("1. Margarita - $300");
+  console.log("2. Pepperoni - $200");
+  console.log("3. Hawaiana - $500");
+  console.log("4. Vegetariana - $400");
+} else if (seleccion.toLowerCase() === "no") {
+  console.log("¡Esperamos verte pronto! ¡Buen día!");
+}
+
+while (seleccion.toLowerCase() === "si") {
+  let opcionPizza = parseInt(prompt("Ingresa el número de la pizza que deseas ordenar:"));
+
+  if (opcionPizza >= 1 && opcionPizza <= 4) {
+    let cantidad = parseInt(prompt(`¿Cuántas pizzas deseas ordenar?`));
+
+    if (cantidad > 0) {
+      let precio = 0;
+
+      switch (opcionPizza) {
+        case 1:
+          precio = 300;
+          break;
+        case 2:
+          precio = 200;
+          break;
+        case 3:
+          precio = 500;
+          break;
+        case 4:
+          precio = 400;
+          break;
       }
+
+      let subtotal = precio * cantidad;
+      total += subtotal;
+      console.log(`Has agregado ${cantidad} pizza(s) al carrito. Subtotal: $${subtotal}`);
     } else {
-      console.log("Opción inválida. Por favor, selecciona un número válido.");
+      console.log("La cantidad ingresada no es válida. Inténtalo nuevamente.");
     }
-  
-    seleccion = prompt("¿Deseas ordenar otra pizza? (SI/NO)");
-  }
-  
-  if (carrito.length > 0) {
-    let total = 0;
-  
-    carrito.forEach((item) => {
-      total += item.subtotal;
-    });
-  
-    console.log(`Total a pagar: $${total.toFixed(2)}. ¡Gracias por tu compra!`);
   } else {
-    console.log("No has agregado ninguna pizza al carrito. ¡Hasta luego!");
+    console.log("Opción inválida. Por favor, selecciona un número válido.");
   }
-  
+
+  seleccion = prompt("¿Deseas ordenar otra pizza? (SI/NO)");
+}
+
+if (total > 0) {
+  console.log(`Total a pagar: $${total.toFixed(2)}. ¡Gracias por tu compra!`);
+} else {
+  console.log("No has agregado ninguna pizza al carrito. ¡Hasta luego!");
+}
