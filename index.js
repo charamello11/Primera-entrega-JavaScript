db4f1c463f9a801fd8916241bbbb811da0dd2230
chara <maximiliano.charamello@gmail.com>
2023-08-14 20:20:08 -0300

    3ra preEntrega - Charamello

--- index.js
+++ index.js
@@ -1,92 +1,134 @@
-// Declaro un array con los datos de las pizzas y sus precios
-const pizzas = [
-  { nombre: 'Margarita', precio: 300 },
-  { nombre: 'Pepperoni', precio: 200 },
-  { nombre: 'Hawaiana', precio: 500 },
-  { nombre: 'Vegetariana', precio: 400 }
-];
+document.addEventListener('DOMContentLoaded', function () {
+  // Creación del header con enlaces
+  const header = document.createElement('header');
+  header.style.backgroundColor = '#333';
+  header.style.color = '#fff';
+  header.style.padding = '10px';
+  header.style.textAlign = 'center';
+  
+  const nav = document.createElement('nav');
+  
+  const ul = document.createElement('ul');
+  ul.style.listStyle = 'none';
+  ul.style.display = 'flex';
+  ul.style.justifyContent = 'center';
+  ul.style.padding = '0';
+  
+  const enlaceInicio = document.createElement('a');
+  enlaceInicio.textContent = 'Inicio';
+  enlaceInicio.href = '#';
+  enlaceInicio.style.color = 'inherit';
+  enlaceInicio.style.textDecoration = 'none';
+  enlaceInicio.style.marginRight = '20px';
+  
+  const enlaceMenu = document.createElement('a');
+  enlaceMenu.textContent = 'Menú';
+  enlaceMenu.href = '#menu';
+  enlaceMenu.style.color = 'inherit';
+  enlaceMenu.style.textDecoration = 'none';
+  enlaceMenu.style.marginRight = '20px';
+  
+  const enlaceCarrito = document.createElement('a');
+  enlaceCarrito.textContent = 'Carrito';
+  enlaceCarrito.href = '#carrito';
+  enlaceCarrito.style.color = 'inherit';
+  enlaceCarrito.style.textDecoration = 'none';
+  
+  ul.appendChild(enlaceInicio);
+  ul.appendChild(enlaceMenu);
+  ul.appendChild(enlaceCarrito);
+  
+  nav.appendChild(ul);
+  header.appendChild(nav);
+  
+  const carritoDiv = document.getElementById('carrito'); 
+  document.body.insertBefore(header, carritoDiv);
+  
+  document.body.insertBefore(header, document.body.firstChild);
 
-let carrito = [];
-
-function mostrarMenu(pizzas) {
-  console.log("A continuación te mostramos nuestro menú de pizzas:");
-  pizzas.forEach((pizza, index) => {
-    console.log(`${index + 1}. ${pizza.nombre} - $${pizza.precio}`);
+  const listaPizzas = document.getElementById('lista-pizzas');
+  const cantidadInput = document.getElementById('cantidad');
+  const agregarAlCarritoBoton = document.getElementById('agregar-al-carrito');
+  const itemsCarrito = document.getElementById('items-carrito');
+  const totalElemento = document.getElementById('total');
+  const realizarPedidoBoton = document.getElementById('realizar-pedido');
+  
+  const pizzas = [
+    { nombre: 'Margarita', precio: 300 },
+    { nombre: 'Pepperoni', precio: 200 },
+    { nombre: 'Hawaiana', precio: 500 }
+  ];
+  
+  let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
+  
+  agregarAlCarritoBoton.addEventListener('click', function () {
+    const indicePizzaSeleccionada = listaPizzas.selectedIndex;
+    const pizzaSeleccionada = pizzas[indicePizzaSeleccionada];
+    const cantidad = parseInt(cantidadInput.value);
+    const subtotal = pizzaSeleccionada.precio * cantidad;
+    
+    carrito.push({ pizza: pizzaSeleccionada.nombre, cantidad, subtotal });
+    actualizarCarrito();
+    guardarCarritoEnLocalStorage();
+    Swal.fire({
+      icon: 'success',
+      title: '¡Pizza agregada!',
+      text: `Has agregado ${cantidad} ${pizzaSeleccionada.nombre} al carrito.`,
+      timer: 2000,
+      timerProgressBar: true
+    });
   });
-}
-
-function obtenerPizzaElegida(pizzas, opcion) {
-  if (opcion >= 1 && opcion <= pizzas.length) {
-    return pizzas[opcion - 1];
-  } else {
-    return null;
-  }
-}
-
-function agregarToppings() {
-  let toppings = [];
-  let toppingsCount = parseInt(prompt(`¿Cuántos toppings deseas agregar?`));
-
-  for (let i = 0; i < toppingsCount; i++) {
-    let topping = prompt(`Ingresa el topping #${i + 1}:`);
-    toppings.push(topping);
-  }
-
-  return toppings;
-}
-
-function agregarPizzaAlCarrito(pizza, cantidad, toppings) {
-  if (cantidad > 0) {
-    let subtotal = pizza.precio * cantidad;
-    carrito.push({ pizza: pizza.nombre, cantidad, toppings, subtotal });
-    console.log(`Has agregado ${cantidad} ${pizza.nombre} al carrito con los siguientes toppings: ${toppings.join(', ')}. Subtotal: $${subtotal}`);
-  } else {
-    console.log("La cantidad ingresada no es válida. Inténtalo nuevamente.");
-  }
-}
-
-let seleccion = prompt("¡Bienvenido a Mistica Pizza! ¿Deseas ordenar una pizza? (SI/NO)");
-
-while (seleccion.toLowerCase() !== "si" && seleccion.toLowerCase() !== "no") {
-  console.log("Por favor, ingresa solo SI o NO.");
-  seleccion = prompt("¿Deseas ordenar una pizza? (SI/NO)");
-}
-
-if (seleccion.toLowerCase() === "si") {
-  mostrarMenu(pizzas);
-} else if (seleccion.toLowerCase() === "no") {
-  console.log("¡Esperamos verte pronto! ¡Buen día!");
-}
-
-while (seleccion.toLowerCase() === "si") {
-  let opcionPizza = parseInt(prompt("Ingresa el número de la pizza que deseas ordenar:"));
-  let pizzaElegida = obtenerPizzaElegida(pizzas, opcionPizza);
-
-  if (pizzaElegida) {
-    let cantidad = parseInt(prompt(`¿Cuántas ${pizzaElegida.nombre} deseas ordenar?`));
-    let toppings = agregarToppings();
-    agregarPizzaAlCarrito(pizzaElegida, cantidad, toppings);
-  } else {
-    console.log("Opción inválida. Por favor, selecciona un número válido.");
-  }
-
-  seleccion = prompt("¿Deseas ordenar otra pizza? (SI/NO)");
-}
-
-if (carrito.length > 0) {
-  let total = 0;
-
-  carrito.forEach((item) => {
-    total += item.subtotal;
+  
+  realizarPedidoBoton.addEventListener('click', function () {
+    let total = 0;
+    carrito.forEach(item => total += item.subtotal);
+    
+    Swal.fire({
+      icon: 'info',
+      title: 'Resumen del Pedido',
+      html: `
+        <ul>
+          ${carrito.map(item => `<li>${item.pizza} x${item.cantidad} - $${item.subtotal}</li>`).join('')}
+        </ul>
+        <p>Total a pagar: $${total}</p>
+      `,
+      confirmButtonText: 'Confirmar Pedido'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // Si se confirma el pedido, borrar el carrito y el almacenamiento local
+        carrito = [];
+        localStorage.removeItem('carrito');
+        actualizarCarrito();
+        
+        Swal.fire({
+          icon: 'success',
+          title: 'Pedido confirmado',
+          text: 'Gracias por tu compra. Vuelva pronto.',
+          timer: 3000,
+          timerProgressBar: true
+        });
+      }
+    });
   });
-
-  console.log("Resumen de tu orden:");
-
-  carrito.forEach((item) => {
-    console.log(`${item.pizza} - Cantidad: ${item.cantidad} - Toppings: ${item.toppings.join(', ')} - Subtotal: $${item.subtotal.toFixed(2)}`);
-  });
-
-  console.log(`Total a pagar: $${total.toFixed(2)}. ¡Gracias por tu compra!`);
-} else {
-  console.log("No has agregado ninguna pizza al carrito. ¡Hasta luego!");
-}
+  
+  function actualizarCarrito() {
+    itemsCarrito.innerHTML = '';
+    let total = 0;
+    
+    carrito.forEach(item => {
+      const li = document.createElement('li');
+      li.textContent = `${item.pizza} x${item.cantidad} - $${item.subtotal}`;
+      itemsCarrito.appendChild(li);
+      total += item.subtotal;
+    });
+    
+    totalElemento.textContent = `Total: $${total}`;
+  }
+  
+  function guardarCarritoEnLocalStorage() {
+    localStorage.setItem('carrito', JSON.stringify(carrito));
+  }
+  
+  // Inicializar el carrito
+  actualizarCarrito();
+});
